4180d663f5c34a6583495430d2ead9983af876f0
chinhbcdev <chinhbcdev@gmail.com>
2023-12-28 17:27:19 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import MenuTabs from "./components/MenuTabs";
 import DexView from "./views/Dex/DexView";
 
-export default function Home() {
+const page = () => {
   return (
     <div>
       <div className="my-12 mt-4 flex justify-center md:hidden"></div>
       <DexView />
     </div>
   );
-}
+};
+
+export default page;
